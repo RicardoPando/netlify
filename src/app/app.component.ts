84f85b873db60fc126134fa101a4933b0882172b
RicardoPando <u201622564@upc.edu.pe>
2023-04-28 23:51:04 -0500

    Revert "democrud"
    This reverts commit 79046518dad6e4d8b932686da5c1d85ad1ad794f.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../demo-styling.css']
 })
 export class AppComponent {
-  title = 'demusCrud';
+  title = 'angular-quickstart';
 }
