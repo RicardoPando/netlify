c119dd0fcb79f621ec20387ee20a29b4f33b2f69
RicardoPando <u201622564@upc.edu.pe>
2023-04-28 23:26:43 -0500

    Revert "template"
    This reverts commit 994efbdea08c8ba5944a560e39d367ea118dee02.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorComponent } from './component/author/author.component';
+import { CreateEditComponent } from './component/author/create-edit/create-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+   path: 'authors', component: AuthorComponent, children: [
+    {
+      path: 'nuevo', component: CreateEditComponent
+    },
+    {
+      path:'edicion/:id', component: CreateEditComponent
+    }
+   ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
